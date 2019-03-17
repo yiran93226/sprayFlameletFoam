e6b47a522ee083212332c0cdb1786bcb1992612d
ZX114 <xu-zhang@sjtu.edu.cn>
2019-03-17 13:49:03 +0800

    Create a table class for a single flamelet table

--- test/table.C
+++ test/table.C
@@ -9,124 +9,50 @@ table::table
     tableFile_(tableFile)
 {
     read();
-    positions_.resize(4);
 }
 
 
-double table::lookupT() const
+void table::find(double Z)
 {
-    double T;
-    double chiL;
-    double chiH;
-    double TL;
-    double TH;
-    if(positions_[3]==0)
+    if (Z > minZ_ && Z < maxZ_)
     {
-        T = ZKey_*480.0 + (1.0-ZKey_)*800;
+        interZ_ = Z;
+        for (size_t i=0; i<lenZ_; i++)
+        {
+            if (Z_[i] > Z)
+            {
+                positionL_ = i-1;
+                positionH_ = i;
+            }
+        }
+        weightL_ = (Z_[positionH_] - interZ_) / (Z_[positionH_] - Z_[positionL_]);
+        weightH_ = (interZ_ - Z_[positionL_]) / (Z_[positionH_] - Z_[positionL_]);
+    }
+    else if(Z <= minZ_)
+    {
+        interZ_ = minZ_;
+        positionL_ = 0;
+        positionH_ = 0;
+        weightL_ = 0.5;
+        weightH_ = 0.5;
     }
     else
     {
-        chiL = ( (Z_[positions_[1]] - ZKey_)*chi_[positions_[0]]
-                + (ZKey_ - Z_[positions_[0]])*chi_[positions_[1]] )
-                / (Z_[positions_[1]] - Z_[positions_[0]]);
-        chiH = ( (Z_[positions_[3]] - ZKey_)*chi_[positions_[2]]
-                + (ZKey_ - Z_[positions_[2]])*chi_[positions_[3]] )
-                / (Z_[positions_[3]] - Z_[positions_[2]]);
-        TL = ( (Z_[positions_[1]] - ZKey_)*T_[positions_[0]]
-                + (ZKey_ - Z_[positions_[0]])*T_[positions_[1]] )
-                / (Z_[positions_[1]] - Z_[positions_[0]]);
-        TH = ( (Z_[positions_[3]] - ZKey_)*T_[positions_[2]]
-                + (ZKey_ - Z_[positions_[2]])*T_[positions_[3]] )
-                / (Z_[positions_[3]] - Z_[positions_[2]]);
-        T = ( (chiH - chiKey_)*TL + (chiKey_ - chiL)*TH ) / (chiH - chiL);
+        interZ_= maxZ_;
+        positionL_ = lenZ_-1;
+        positionH_ = lenZ_-1;
+        weightL_ = 0.5;
+        weightH_ = 0.5;
     }
-    return T;
-}
-
-
-double table::lookupY(size_t i) const
-{
-    double Y;
-    double chiL;
-    double chiH;
-    double YL;
-    double YH;
-    if(positions_[3]==0)
-    {
-        Y = ZKey_*480.0 + (1.0-ZKey_)*800;
-    }
-    else
-    {
-        chiL = ( (Z_[positions_[1]] - ZKey_)*chi_[positions_[0]]
-                + (ZKey_ - Z_[positions_[0]])*chi_[positions_[1]] )
-                / (Z_[positions_[1]] - Z_[positions_[0]]);
-        chiH = ( (Z_[positions_[3]] - ZKey_)*chi_[positions_[2]]
-                + (ZKey_ - Z_[positions_[2]])*chi_[positions_[3]] )
-                / (Z_[positions_[3]] - Z_[positions_[2]]);
-        YL = ( (Z_[positions_[1]] - ZKey_)*Y_[i][positions_[0]]
-                + (ZKey_ - Z_[positions_[0]])*Y_[i][positions_[1]] )
-                / (Z_[positions_[1]] - Z_[positions_[0]]);
-        YH = ( (Z_[positions_[3]] - ZKey_)*Y_[i][positions_[2]]
-                + (ZKey_ - Z_[positions_[2]])*Y_[i][positions_[3]] )
-                / (Z_[positions_[3]] - Z_[positions_[2]]);
-        Y = ( (chiH - chiKey_)*YL + (chiKey_ - chiL)*YH ) / (chiH - chiL);
-    }
-    return Y;
-}
-
-
-void table::find(double Z, double chi)
-{
-    std::vector<size_t> interPos;
-    double interChi;
-
-    if(Z <= 0) Z = 0;
-    if(Z >= 1) Z = 1;
-    ZKey_ = Z;
-    chiKey_ = chi;
-    for(size_t i=0;i<Z_.size();i++)
-    {
-        if(Z_[i]<=Z)
-        {
-            interPos.push_back(i);
-            for(size_t j=i;j<Z_.size();j++)
-                if(Z_[j]>Z) 
-                {
-                    i=j-1;
-                    break;
-                }
-        }
-    }
-    for(size_t i=0;i<interPos.size();i++)
-    {
-        // Linear interpolate chi
-        interChi = ( (Z_[interPos[i]] - Z)*chi_[interPos[i]-1]
-                    + (Z - Z_[interPos[i]-1])*chi_[interPos[i]] )
-                    / (Z_[interPos[i]] - Z_[interPos[i]-1]);
-        if(interChi>chi)
-        {
-            positions_[3] = interPos[i];
-            positions_[2] = interPos[i]-1;
-            positions_[1] = interPos[i-1];
-            positions_[0] = interPos[i-1]-1;
-            break;
-        }
-        if(i==interPos.size()-1)
-        {
-            positions_[3] = 0;
-            positions_[2] = 0;
-            positions_[1] = 0;
-            positions_[0] = 0;
-        }
-    }
+    interYc_ = Yc_[positionH_] * weightH_ + Yc_[positionL_] * weightL_;
 }
 
 
 void table::read()
 {
     std::string line, str;
-    std::getline(tableFile_, str); // The first line
-    nColumn_ = std::count(str.begin(), str.end(), ',') + 1;
+    std::getline(tableFile_, firstLine_); // The first line
+    nColumn_ = std::count(firstLine_.begin(), firstLine_.end(), ',') + 1;
     std::cout << "#Number of columns:\t" << nColumn_ << std::endl;
     Y_.resize(nColumn_-3);
     while(std::getline(tableFile_,line))
@@ -140,14 +66,16 @@ void table::read()
             else if(n == 0)
                 Z_.push_back(std::stod(str));
             else if(n == 1)
-                chi_.push_back(std::stod(str));
+                Yc_.push_back(std::stod(str));
             else
                 T_.push_back(std::stod(str));
             n++;
         }
     }
+    lenZ_ = Z_.size();
+    maxZ_ = Z_[lenZ_-1];
+    minZ_ = Z_[0];
     std::cout << "#Number of species:\t" << Y_.size() << std::endl;
     std::cout << "#Length of Z:\t" << Z_.size() << std::endl;
-    std::cout << "#Length of Yi:\t" << Y_[0].size() << std::endl;
     std::cout << "#Length of T:\t" << T_.size() << std::endl;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,46 @@
 #include "table.H"
-#include "table.C"
 using namespace std;
 
 int main()
 {
-    double T;
-    ifstream flameletTableFile("flameletTable.csv");
+    std::string filename;
+    filename = "flameletTable.csv";
+    std::ifstream flameletTableFile(filename);
+    std::ifstream sprayFlameFile("sprayTable.csv");
+    std::ofstream outfile("lookupResult.csv");
     table flameletTable(flameletTableFile);
-    flameletTable.find(0.000196492,0.000510477);
-    T = flameletTable.lookupT();
-    cout << T << endl;
+    table sprayFlame(sprayFlameFile);
+
+    double nsp = flameletTable.getNsp();
+    std::vector<double> Z = sprayFlame.getZ();
+    std::vector<double> Yc = sprayFlame.getYc();
+    std::vector<double> T(Z.size());
+    std::vector<std::vector<double> > Y(nsp);
+    for(size_t i=0; i<nsp; i++)
+    {
+        Y[i].resize(Z.size());
+    }
+    for(size_t i=0; i<Z.size(); i++)
+    {
+        flameletTable.find(Z[i]);
+        T[i] = flameletTable.lookupT();
+        for(size_t j=0; j<nsp; j++)
+        {
+            Y[j][i] = flameletTable.lookupY(j);
+        }
+    }
+
+    outfile << flameletTable.getFirstLine() << std::endl;
+    for(size_t i=0; i<Z.size(); i++)
+    {
+        outfile << Z[i] << "," << Yc[i] << "," << T[i] << ",";
+        for(size_t j=0; j<nsp; j++)
+        {
+            outfile << Y[j][i];
+            if (j!=nsp-1) outfile << ",";
+        }
+        outfile << std::endl;
+    }
+
     return 0;
 }
