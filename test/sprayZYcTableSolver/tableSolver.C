1b2d1d1d5bed26f6273ee57f96fdf53738c75112
ZX114 <xu-zhang@sjtu.edu.cn>
2019-03-21 18:36:01 +0800

    Clock tableSolver

---
+++ test/sprayZYcTableSolver/tableSolver.C
@@ -0,0 +1,69 @@
+#include "tableSolver.H"
+
+tableSolver::tableSolver()
+{
+    collectTables();
+}
+
+void tableSolver::collectTables()
+{
+    size_t n=0;
+    while(true)
+    {
+        std::ifstream flameletTableFile("tablesYc/sprayTable_"+std::to_string(n)+".csv");
+        if (!flameletTableFile) break;
+        table* tTable = new table(flameletTableFile);
+        tables_.push_back(tTable);
+        n++;
+    }
+    tableNum_ = tables_.size();
+}
+
+void tableSolver::find(double Z, double Yc)
+{
+    std::vector<double> Ycs(tableNum_-1);
+    std::map<double, size_t> YcMap;
+    for (size_t i=0; i<tableNum_; i++)
+    {
+        tables_[i]->find(Z);
+        if (i!=tableNum_-1)
+        {
+            Ycs[i] = tables_[i]->lookupYc();
+            YcMap.insert({Ycs[i], i});
+        }
+    }
+    std::sort(Ycs.begin(), Ycs.end());
+    double YcH, YcL;
+    if ( (Yc > Ycs[0]) && (Yc < Ycs[tableNum_-2]) )
+    {
+        for (size_t i=0; i<tableNum_; i++)
+        {
+            if (Ycs[i] > Yc)
+            {
+                YcH = Ycs[i];
+                YcL = Ycs[i-1];
+                break;
+            }
+        }
+        weightL_ = (YcH - Yc) / (YcH - YcL);
+        weightH_ = (Yc - YcL) / (YcH - YcL);
+        positionL_ = YcMap[YcL];
+        positionH_ = YcMap[YcH];
+    }
+    else if (Yc <= Ycs[0])
+    {
+        weightL_ = 0.5;
+        weightH_ = 0.5;
+        positionL_ = tableNum_-1;
+        positionH_ = tableNum_-1;
+    }
+    else
+    {
+        YcH = Ycs[tableNum_-1];
+        YcL = Ycs[tableNum_-1];
+        weightL_ = 0.5;
+        weightH_ = 0.5;
+        positionL_ = YcMap[YcL];
+        positionH_ = YcMap[YcH];
+    }
+}
