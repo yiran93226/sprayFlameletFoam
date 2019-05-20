9994a3f19a19e808d3f1a699009909a9c8a1d121
ZX114 <xu-zhang@sjtu.edu.cn>
2019-05-20 16:29:18 +0800

    Use exponential distribution

--- src/flameletLib/tableSolver.C
+++ src/flameletLib/tableSolver.C
@@ -2,6 +2,7 @@
 
 tableSolver::tableSolver(size_t zetaIndex)
 {
+    useExp_ = false;
     collectTables(zetaIndex);
 }
 
@@ -16,11 +17,16 @@ void tableSolver::collectTables(size_t zetaIndex)
         tables_.push_back(tTable);
         n++;
     }
+
     tableNum_ = tables_.size();
+    nsp_ = tables_[0]->getNsp();
+    expY_.resize(nsp_,0.0);
 }
 
 void tableSolver::find(double Z, double Yc)
 {
+    useExp_ = false;
+
     std::vector<double> Ycs(tableNum_-1);
     std::map<double, size_t> YcMap;
     for (size_t i=0; i<tableNum_; i++)
@@ -57,10 +63,36 @@ void tableSolver::find(double Z, double Yc)
         // weightH_ = 0.5;
         // positionL_ = tableNum_-1;
         // positionH_ = tableNum_-1;
-        weightL_ = (Ycs[0] - Yc) / (Ycs[0] - 0);
-        weightH_ = (Yc - 0) / (Ycs[0] - 0);
+        // weightL_ = (Ycs[0] - Yc) / (Ycs[0] - 0);
+        // weightH_ = (Yc - 0) / (Ycs[0] - 0);
+        // positionL_ = tableNum_-1;
+        // positionH_ = tableNum_-2;
+        YcH = Ycs[0];
+        YcL = 0.0;
+        positionH_ = YcMap[YcH];
         positionL_ = tableNum_-1;
-        positionH_ = tableNum_-2;
+
+        // T
+        double B = tables_[positionL_]->lookupT() - 1.0;
+        double A = std::log(tables_[positionH_]->lookupT() - B)
+                    / (Ycs[0]);
+        expT_ = std::exp(A*Yc) + B;
+
+        // omegaYc
+        B = tables_[positionL_]->lookupOmegaYc() - 1.0;
+        A = std::log(tables_[positionH_]->lookupOmegaYc() - B)
+            / (Ycs[0]);
+        expOmegaYc_ = std::exp(A*Yc) + B;
+
+        // Y
+        for (size_t i=0; i<nsp_; i++) {
+            B = tables_[positionL_]->lookupY(i) - 1.0;
+            A = std::log(tables_[positionH_]->lookupY(i) - B)
+                / (Ycs[0]);
+            expY_[i] = std::exp(A*Yc) + B;
+        }
+
+        useExp_ = true;
     }
     else
     {
